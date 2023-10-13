ed6fae9062d5712413a5e1b1b8fb0df99d7d8f41
ZippyZipZop <145229762+ZippyZipZop@users.noreply.github.com>
2023-10-13 11:06:49 -0400

    update constants.php

--- constants.php
+++ constants.php
@@ -100,4 +100,21 @@
         case ZONE;
         case PERSONAL;
     }
+    
+    enum MonsterClass {
+        case GIANT;       /* 2-3x HP */
+        case MINI;        /* 1/2 - 1/4 HP */
+        case HARDENED;    /* 3-4x DEF */
+        case ENRAGED;     /* 4x STR, -5% HP */
+        case EXPERIENCED; /* 3x EXP, +2% HP+DEF */
+        case PACKRAT;     /* Item rarity chances increased/1.5x, 3x gold */
+        case DEFECTING;
+        case LEADER;
+    }
+    
+    enum ItemRarity {
+       case WORTHLESS = 70.0;
+       /* ... */
+       case GODLY     = 0.005;
+    }
 ?>
